50429adeb9099c440c7576ae01548cbca62c900f
2018김수종 <tnwhd6268@naver.com>
2021-05-20 16:49:41 +0900

    12주차

--- New Unity Project/Assets/Script/Player.cs
+++ New Unity Project/Assets/Script/Player.cs
@@ -82,6 +82,7 @@ public class Player : MonoBehaviour
 
     private void Fire()
     {
+        GetComponent<AudioSource>().Play();
         float direction = transform.localScale.x;
         Quaternion quaternion = new Quaternion(0, 0, 0, 0);
 
